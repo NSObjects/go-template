086bc32d2c6a03d065a3f88c8c6898b2a631c806
lintao <mrqter@gmail.com>
2026-06-19 10:38:04 +0800

    Wire business modules in boot

--- internal/boot/business.go
+++ internal/boot/business.go
@@ -1,5 +1,22 @@
 package boot
 
+import (
+	"context"
+
+	"github.com/samber/do/v2"
+
+	customermemory "github.com/NSObjects/go-template/internal/modules/customer/adapters/memory"
+	customerhttp "github.com/NSObjects/go-template/internal/modules/customer/http"
+	customerusecase "github.com/NSObjects/go-template/internal/modules/customer/usecase"
+	productmemory "github.com/NSObjects/go-template/internal/modules/product/adapters/memory"
+	producthttp "github.com/NSObjects/go-template/internal/modules/product/http"
+	productusecase "github.com/NSObjects/go-template/internal/modules/product/usecase"
+	salesordermemory "github.com/NSObjects/go-template/internal/modules/salesorder/adapters/memory"
+	salesorderhttp "github.com/NSObjects/go-template/internal/modules/salesorder/http"
+	salesorderusecase "github.com/NSObjects/go-template/internal/modules/salesorder/usecase"
+	"github.com/NSObjects/go-template/internal/platform/apperr"
+)
+
 // BusinessModules returns the business modules installed by the default runtime.
 func BusinessModules() []Module {
 	return []Module{
@@ -7,4 +24,132 @@ func BusinessModules() []Module {
 		productModule(),
 		salesOrderModule(),
 	}
+}
+
+func customerModule() Module {
+	return NewModule("customer",
+		Provide(newCustomerStore),
+		Provide(newCustomerUsecase),
+		Provide(newCustomerHandler),
+		Route(customerhttp.Register),
+	)
+}
+
+func newCustomerStore(do.Injector) (*customermemory.Store, error) {
+	return customermemory.NewStore(), nil
+}
+
+func newCustomerUsecase(i do.Injector) (*customerusecase.Usecase, error) {
+	store, err := do.Invoke[*customermemory.Store](i)
+	if err != nil {
+		return nil, err
+	}
+	return customerusecase.New(store), nil
+}
+
+func newCustomerHandler(i do.Injector) (*customerhttp.Handler, error) {
+	usecase, err := do.Invoke[*customerusecase.Usecase](i)
+	if err != nil {
+		return nil, err
+	}
+	return customerhttp.New(usecase), nil
+}
+
+func productModule() Module {
+	return NewModule("product",
+		Provide(newProductStore),
+		Provide(newProductUsecase),
+		Provide(newProductHandler),
+		Route(producthttp.Register),
+	)
+}
+
+func newProductStore(do.Injector) (*productmemory.Store, error) {
+	return productmemory.NewStore(), nil
+}
+
+func newProductUsecase(i do.Injector) (*productusecase.Usecase, error) {
+	store, err := do.Invoke[*productmemory.Store](i)
+	if err != nil {
+		return nil, err
+	}
+	return productusecase.New(store), nil
+}
+
+func newProductHandler(i do.Injector) (*producthttp.Handler, error) {
+	usecase, err := do.Invoke[*productusecase.Usecase](i)
+	if err != nil {
+		return nil, err
+	}
+	return producthttp.New(usecase), nil
+}
+
+func salesOrderModule() Module {
+	return NewModule("sales-order",
+		Provide(newSalesOrderStore),
+		Provide(newSalesOrderUsecase),
+		Provide(newSalesOrderHandler),
+		Route(salesorderhttp.Register),
+	)
+}
+
+func newSalesOrderStore(do.Injector) (*salesordermemory.Store, error) {
+	return salesordermemory.NewStore(), nil
+}
+
+func newSalesOrderUsecase(i do.Injector) (*salesorderusecase.Usecase, error) {
+	store, err := do.Invoke[*salesordermemory.Store](i)
+	if err != nil {
+		return nil, err
+	}
+	customers, err := do.Invoke[*customerusecase.Usecase](i)
+	if err != nil {
+		return nil, err
+	}
+	products, err := do.Invoke[*productusecase.Usecase](i)
+	if err != nil {
+		return nil, err
+	}
+	return salesorderusecase.New(
+		store,
+		salesOrderCustomerLookup{customers: customers},
+		salesOrderProductLookup{products: products},
+	), nil
+}
+
+func newSalesOrderHandler(i do.Injector) (*salesorderhttp.Handler, error) {
+	usecase, err := do.Invoke[*salesorderusecase.Usecase](i)
+	if err != nil {
+		return nil, err
+	}
+	return salesorderhttp.New(usecase), nil
+}
+
+type salesOrderCustomerLookup struct {
+	customers *customerusecase.Usecase
+}
+
+func (l salesOrderCustomerLookup) CustomerExists(ctx context.Context, id int64) (bool, error) {
+	if _, err := l.customers.Get(ctx, id); err != nil {
+		if appErr, ok := apperr.Parse(err); ok && appErr.Code() == apperr.ErrNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+type salesOrderProductLookup struct {
+	products *productusecase.Usecase
+}
+
+func (l salesOrderProductLookup) FindProduct(ctx context.Context, id int64) (salesorderusecase.ProductSnapshot, error) {
+	product, err := l.products.Get(ctx, id)
+	if err != nil {
+		if appErr, ok := apperr.Parse(err); ok && appErr.Code() == apperr.ErrNotFound {
+			return salesorderusecase.ProductSnapshot{Exists: false}, nil
+		}
+		return salesorderusecase.ProductSnapshot{}, err
+	}
+	return salesorderusecase.ProductSnapshot{Exists: true, Active: product.Active}, nil
 }
